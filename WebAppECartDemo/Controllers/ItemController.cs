da6f8be965284c77e6fbc0952d74828f5e2be1d7
ajitbodkhe <64258389+ajitbodkhe@users.noreply.github.com>
2020-06-03 19:35:48 +0530

    Add project files.

---
+++ WebAppECartDemo/Controllers/ItemController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
+
+namespace WebAppECartDemo.Controllers
+{
+    public class ItemController : Controller
+    {
+        WebAppECartDemo_dbEntities eCartDBEntities;
+
+        public ItemController()
+        {
+            eCartDBEntities = new WebAppECartDemo_dbEntities();
+        }
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel itemViewModel = new ItemViewModel();
+            itemViewModel.categoryItemList = from itemObj in eCartDBEntities.Categories
+                                             select new SelectListItem
+                                             {
+                                                 Text = itemObj.CategoryName,
+                                                 Value = itemObj.CategoryId.ToString(),
+                                                 Selected = true
+
+                                             };
+            return View(itemViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddItem(ItemViewModel itemViewModel)
+        {
+            string NewImage = Guid.NewGuid() + Path.GetExtension(itemViewModel.ImagePath.FileName);
+            itemViewModel.ImagePath.SaveAs(Server.MapPath("~/Images/"+NewImage));
+
+            Item objItem = new Item();
+            objItem.ImagePath = "~/Images/" + NewImage;
+            objItem.CategoryId = itemViewModel.CategoryId;
+            objItem.Description = itemViewModel.Description;
+            objItem.ItemCode = itemViewModel.ItemCode;
+            objItem.ItemId = Guid.NewGuid();
+            objItem.ItemName = itemViewModel.ItemName;
+            objItem.ItemPrice = itemViewModel.ItemPrice;
+            eCartDBEntities.Items.Add(objItem);
+            eCartDBEntities.SaveChanges();
+
+            return Json(new { Success = true, Message = "Item Added Successfully", JsonRequestBehavior.AllowGet });
+        }
+    }
+}
---
+++ WebAppECartDemo/Controllers/ShoppingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
+
+namespace WebAppECartDemo.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        WebAppECartDemo_dbEntities eCartDBEntities;
+
+        public ShoppingController()
+        {
+            eCartDBEntities = new WebAppECartDemo_dbEntities();
+        }
+
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            IEnumerable<ShoppingViewModel> listShoppingViewModel = (from objItem in eCartDBEntities.Items
+                                                                    join objCate in eCartDBEntities.Categories
+                                                                    on objItem.CategoryId equals objCate.CategoryId
+                                                                    select new ShoppingViewModel
+                                                                    {
+                                                                        ImagePath = objItem.ImagePath,
+                                                                        ItemName = objItem.ItemName,
+                                                                        Description = objItem.Description,
+                                                                        ItemPrice = objItem.ItemPrice,
+                                                                        ItemCode = objItem.ItemCode,
+                                                                        ItemId = objItem.ItemId,
+                                                                        Category = objCate.CategoryName
+                                                                    }
+                                                                    ).ToList();
+            return View(listShoppingViewModel);
+        }
+    }
+}
